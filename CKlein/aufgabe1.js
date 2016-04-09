7fd0c578e2f587d7204a92283d66acf6c57d33b7
Corinna Klein <corinna_sofie.klein@smail.fh-koeln.de>
2016-04-09 19:09:42 +0200

    Aufgabe1 geloest

--- CKlein/aufgabe1.js
+++ CKlein/aufgabe1.js
@@ -1,3 +1,17 @@
-/* Hello World! program in Node.js */
-console.log("Hello World!");
+var fs = require('fs');
+var obj;
+fs.readFile('wolkenkratzer.json', 'utf8', function (err, data) {
+  if (err) throw err;
+  obj = JSON.parse(data);
 
+    console.log("Alle Wolkenkratzer:  ");
+    for ( var x=0; x<obj.wolkenkratzer.length; x++)
+    {
+        console.log("Name:  "+obj.wolkenkratzer[x].name);
+        console.log("Stadt:  "+obj.wolkenkratzer[x].stadt);
+        console.log("Hoehe:  "+obj.wolkenkratzer[x].hoehe);
+        console.log("------------------------------------");
+    }
+});
+
+//console.log("vielleicht "+obj.wolkenkratzer[0].stadt);
