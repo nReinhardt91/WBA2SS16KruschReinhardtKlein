a40d6f7a191f557418283251b2669771b0eae725
Janice Bulk <Janice Bulk>
2016-04-27 14:00:42 +0200

    MKrusch Aufgabe 2

---
+++ MKrusch/nr2.js
@@ -0,0 +1,21 @@
+var fs = require('fs');
+var chalk = require('chalk');
+var obj;
+
+var chalk_red = chalk.red;
+var chalk_green = chalk.green;
+var chalk_yellow = chalk.yellow;
+
+
+fs.readFile('wolkenkratzer.json', 'utf8', function (err, data) {
+  if(err) throw err;
+  obj =JSON.parse(data);
+
+  console.log("Alle Wolkenkratzer:  ")
+  for (var x=0; x<obj.wolkenkratzer.length; x++){
+    console.log(chalk_red("Name:  "+obj.wolkenkratzer[x].name));
+    console.log(chalk_green("Stadt:  "+obj.wolkenkratzer[x].stadt));
+    console.log(chalk_yellow("Höhe:  "+obj.wolkenkratzer[x].hoehe));
+    console.log("----------------------------------");
+  }
+});
