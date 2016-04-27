7b9d017d4dca131f854199c76f3104dd3e988a2d
Janice Bulk <Janice Bulk>
2016-04-27 13:52:47 +0200

    MKrusch 1. Aufgabe

---
+++ MKrusch/nr1.js
@@ -0,0 +1,14 @@
+var fs = require('fs');
+var obj;
+fs.readFile('wolkenkratzer.json', 'utf8', function (err, data) {
+  if(err) throw err;
+  obj =JSON.parse(data);
+
+  console.log("Alle Wolkenkratzer:  ")
+  for (var x=0; x<obj.wolkenkratzer.length; x++){
+    console.log("Name:  "+obj.wolkenkratzer[x].name);
+    console.log("Stadt:  "+obj.wolkenkratzer[x].stadt);
+    console.log("Höhe:  "+obj.wolkenkratzer[x].hoehe);
+    console.log("----------------------------------");
+  }
+});
