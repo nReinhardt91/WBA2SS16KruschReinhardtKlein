994a19b83d3c3825dc138a444a862316b96c135b
Corinna Klein <corinna_sofie.klein@smail.fh-koeln.de>
2016-05-21 14:37:16 +0200

    post-methode

--- Projekt/app.js
+++ Projekt/app.js
@@ -4,7 +4,7 @@ var bodyParser=require('body-parser');
 var jsonParser=bodyParser.json();
 
 var app=express();
-
+app.use(jsonParser);
 var serverPort=1337;
 
 var recipes = [
@@ -13,22 +13,6 @@ var recipes = [
     {name: "Auflauf", preparation: "aufwendig", level: 3}
 ];
 
-//content-negotiation
-/*
-app.get('/', function(req, res){
-var acceptetTypes=req.accepts(['html', 'json']);
-    switch(acceptetTypes){
-    case 'html': 
-            res.type('html').send('<h1>HelloWorld</h1>');
-            break;
-    case 'json':
-            res.json(recipes);
-            break;
-    default: 
-        res.status(406).end();
-    }
-});
-*/
 
 
 //get Methode -> Filtern anhand Schwierigkeitsgrad
@@ -47,8 +31,35 @@ app.get('/recipes', function(req, res){
     }
 });
 
+//Post
+// URL http://localhost:1337/recipe
+app.post('/recipe', jsonParser, function(req, res){
+    var newRecipe=req.body;
+  console.log(newRecipe.name);      // your JSON
+  res.send('Funktioniert' + newRecipe.name);    // echo the result back
+});
 
 
 app.listen(serverPort, function(){
     console.log('App listens on Port '+serverPort);
 });
+
+
+
+
+//content-negotiation
+/*
+app.get('/', function(req, res){
+var acceptetTypes=req.accepts(['html', 'json']);
+    switch(acceptetTypes){
+    case 'html': 
+            res.type('html').send('<h1>HelloWorld</h1>');
+            break;
+    case 'json':
+            res.json(recipes);
+            break;
+    default: 
+        res.status(406).end();
+    }
+});
+*/
