c5440f686b16f2bb9653169dc05c8522bbef49cf
Janice Bulk <Janice Bulk>
2016-06-27 17:35:22 +0200

    requestjs eingefügt

--- Projekt/Postimplementierung/Dienstnutzer/app.js
+++ Projekt/Postimplementierung/Dienstnutzer/app.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 var bodyParser = require('body-parser');
 var http = require('http');
 var ejs = require('ejs');
+var request = require('request');
 
 var jsonParser = bodyParser.json();
 var app = express();
@@ -49,36 +50,50 @@ app.post('/addRezept', function(req, res) {
     "preparation": req.body.zubereitung,
     "level":req.body.level,
   };
-
-  rezept.name = req.body.name;
-  rezept.preparation = req.body.zubereitung;
-  rezept.level = req.body.level;
-
-  var rezeptj = JSON.stringify(rezept);
-
-  var request = http.request(options, function(response) {
-  console.log('STATUS: ' + response.statusCode);
-  console.log('HEADERS: ' + JSON.stringify(response.headers));
-  response.setEncoding('utf8');
-  response.on('data', function (chunk) {
-    console.log('BODY: ' + chunk);
-  });
+  request.post(
+      "http://localhost:3000" + '/addRezept', {
+          json: rezept
+      , }
+      , function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+              console.log(body);
+              res.status(200).send('OK');
+          } else {
+              handleInternalError(req, res);
+          };
+      });
 });
-
-// write data to request body
-console.log(JSON.stringify(rezept));
-request.write(rezeptj);
-request.end();
-
-  console.log("reqbody: "+req.body.name)
-  console.log(rezeptj);
-
-  fs.readFile('./Pages/addRezept.html', function(err, page) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(page);
-    res.end();
-   });
-});
+//   rezept.name = req.body.name;
+//   rezept.preparation = req.body.zubereitung;
+//   rezept.level = req.body.level;
+//
+//   var rezeptj = JSON.stringify(rezept);
+//
+//   var request = http.request(options, function(response) {
+//   console.log('STATUS: ' + response.statusCode);
+//   console.log('HEADERS: ' + JSON.stringify(response.headers));
+//   response.setEncoding('utf8');
+//   response.on('data', function (chunk) {
+//     console.log('BODY: ' + chunk);
+//
+//   });
+//
+// });
+// request.write(rezeptj);
+// request.end();
+// // write data to request body
+// console.log(JSON.stringify(rezept));
+//
+//
+//   console.log("reqbody: "+req.body.name)
+//   console.log(rezeptj);
+//
+//   fs.readFile('./Pages/addRezept.html', function(err, page) {
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     res.write(page);
+//     res.end();
+//    });
+// });
 
 // Alle rezepte ausgeben--------------------------------------------------------
 app.get('/rezepte', function(req,res){
